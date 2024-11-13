8a22f6526b9d35cf67323e7254fcdf8da44c0028
Xavier Nishikawa <xavier.nishikawa@gmail.com>
2024-11-12 23:49:43 -0500

    Calculates val for model

--- trainMore.py
+++ trainMore.py
@@ -2,8 +2,12 @@ import os
 
 from ultralytics import YOLOv10 
 
-config_path = '/home/xavier/Robolab/config.yaml'
+config_path = '//home/xavier/Robolab/config.yaml'
 
-model = YOLOv10('/home/xavier/Robolab/runs/detect/train13/weights/last.pt')
+# model = YOLOv10('/home/xavier/Robolab/runs/detect/train13/weights/last.pt')
 
-model.train(data=config_path, epochs = 100, batch = 16) 
+#model.train(data=config_path, epochs = 100, batch = 16) 
+
+
+model= YOLOv10('/home/xavier/Robolab/runs/detect/train14/weights/last.pt')
+model.val()
